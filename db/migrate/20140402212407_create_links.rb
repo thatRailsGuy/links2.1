e1744c52188fc3ec93bbe3db2eda3b9534dd87b6
Prelang Builder <builder@prelang.com>
2014-04-02 21:24:08 +0000

    Generated model Link with migration

---
+++ db/migrate/20140402212407_create_links.rb
@@ -0,0 +1,12 @@
+class CreateLinks < ActiveRecord::Migration
+  def change
+    create_table :links do |t|
+      t.references :user, index: true
+      t.string :url
+      t.string :title
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
